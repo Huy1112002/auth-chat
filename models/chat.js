dcdce98cad92b89b91967d61c654ebc159be11b1
Huy1112002 <93692689+Huy1112002@users.noreply.github.com>
2022-12-14 17:20:10 +0700

    Update chat.js

--- models/chat.js
+++ models/chat.js
@@ -27,7 +27,7 @@ const getChatMessagesOfUser = async(username ,result) => {
     var pool = await conn
     var sqlString = 'SELECT * FROM messages WHERE src_user = @username OR des_user = @username';
     return await pool.request()
-    .input('username', sql.varchar(50), SSN)
+    .input('username', sql.varchar(50), username)
     .query(sqlString, function(err, data){
         if(data.recordset.length > 0){
             result(null, data.recordset)
